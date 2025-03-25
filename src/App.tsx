b9e9cae9be4fbf3e8afc8cef1245a14844d83f63
Bobur590 <torsunovbobur7@example.com>
2025-03-25 05:56:28 +0500

    full fayl strakchi

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import NotFound from './pages/NotFound'
 
 function App() {
   return (<>
     <Routes>
-      <Route/>
+      <Route path='/' element={<Home/>} />
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/*' element={<NotFound/>}/>
     </Routes>
   </>
   )
