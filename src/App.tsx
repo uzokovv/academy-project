e3f0e30c138674890bf2823af5e5136b9aecb5b8
Bobur590 <torsunovbobur7@example.com>
2025-03-25 14:10:36 +0500

    404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
-import Contact from './pages/contact'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 import NotFound from './pages/NotFound'
 
 function App() {
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import notFoundImage from '../assets/images/404.png';
 
 function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <div className="w-screen h-screen flex justify-center items-center bg-black">
+      <img className="w-full h-full object-cover" src={notFoundImage} alt="404" />
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
